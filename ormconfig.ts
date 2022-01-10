bf6014c311c293a2ac92bdde0d39721aa2ba3775
Milos Vuksanovic <hex3solutions@gmail.com>
2022-01-10 15:51:29 +0100

    Configure typeorm connection

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,10 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const typeormConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
+  type: 'mysql',
+  host :'database-2.c1ymnux6g6xr.us-east-1.rds.amazonaws.com',
+  database: 'db1',
+  port: 3306,
   username: 'user_1',
-  password: 'test1',
-  database: 'typeorm-demo-db',
+  password: 'testtest1',
   entities: ['dist/src/**/*.entity.js'],
   synchronize: false,
   migrations: ['dist/src/db/migrations/*.js'],
@@ -13,5 +15,3 @@ const typeormConfig: TypeOrmModuleOptions = {
   },
 };
 export default typeormConfig;
-
-module.exports = typeormConfig;
