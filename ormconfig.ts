7cbfff3e9450a42dd3a60557c24283475d7f7691
Milos Vuksanovic <hex3solutions@gmail.com>
2022-01-26 11:54:46 +0100

    Add connection options to typeorm config file

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,12 +3,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 const typeormConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host :'database-2.c1ymnux6g6xr.us-east-1.rds.amazonaws.com',
-  database: 'db1',
+  database: 'db555',
+  keepConnectionAlive: true,
   port: 3306,
   username: 'user_1',
   password: 'testtest1',
   entities: ['dist/src/**/*.entity.js'],
-  synchronize: true,
+  synchronize: false,
   migrations: ['dist/src/db/migrations/*.js'],
   cli: {
     migrationsDir: 'src/db/migrations',
